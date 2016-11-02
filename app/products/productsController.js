0a75cad68fd9034e8ba4697e979c9e951845b686
murarivarma <murarivarma@hotmail.com>
2016-11-03 00:12:52 +0530

    Created JSON files for productdata, countriesList. - Asynchronous calls  - promises and web services -

--- app/products/productsController.js
+++ app/products/productsController.js
@@ -2,8 +2,13 @@
     
     function productsCtrlFunction($scope, productsSvc){
         
-        $scope.productsList = productsSvc.getproducts();
+        //$scope.productsList = productsSvc.getproducts();
         
+        productsSvc.getproductsFromAPI().then(function(response){
+            $scope.productsList = response.data.products;
+        }).catch(function(errorResponse){
+            $scope.showError = true;
+        });
     }
     
     angular.module("products").controller("productsCtrl", ["$scope", "productsSvc", productsCtrlFunction]);
--- app/register/registerController.js
+++ app/register/registerController.js
@@ -3,7 +3,13 @@
     function registerCtrlAction($scope, lookupSvc) {
         $scope.user = {};
         
-        $scope.countries = lookupSvc.getCountriesList();
+        //$scope.countries = lookupSvc.getCountriesList();
+        
+        lookupSvc.getCountriesListFromAPI().then(function(response){
+            $scope.countries = response.data.countries;
+        }).catch(function(errorResponse){
+            console.log(errorResponse);
+        });
         
         $scope.loadStatesByCountry = function(){
           var countryCode = $scope.user.country.code;
