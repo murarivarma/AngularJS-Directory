a2c14b3cbb2005c772d0fd4f84c5e8e191fd1037
murarivarma <murarivarma@hotmail.com>
2016-11-08 19:43:49 +0530

    Custom Directive  customClick replicating ng-click

--- app/customDirectives/customDirectives.js
+++ app/customDirectives/customDirectives.js
@@ -11,11 +11,27 @@
         return {
             templateUrl:"app/customDirectives/customHeader.html",
             restrict:"A"
-        }
+        };
+    }
+    
+    function customClickFunction(){
+        return {
+            restrict:"A",
+            link: function(scope, element, attrs){
+                
+                element.bind("click", function(){
+                    var fun = attrs["customClick"];
+                    scope.$eval(fun);
+                });
+                
+            }
+        };
     }
     
     
     angular.module("customDirective",[]);
-    angular.module("customDirective",[]).directive("customLogo",[customLogoFunction])
-    .directive("customHeader", [customHeaderFunction]);
+    angular.module("customDirective",[])
+        .directive("customLogo",[customLogoFunction])
+        .directive("customHeader", [customHeaderFunction])
+        .directive("customClick", [customClickFunction]);
 })();
